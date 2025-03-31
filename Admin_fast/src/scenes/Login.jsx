5aa6981fb3decddaa7bbbcfa73b3f13528c92b94
Infusion810 <infusionpvtltd@gmail.com>
2025-03-31 19:19:18 +0530

    Update Login.jsx

--- Admin_fast/src/scenes/Login.jsx
+++ Admin_fast/src/scenes/Login.jsx
@@ -13,7 +13,7 @@ function Login({ setIsAuthenticated }) {
         e.preventDefault();
 
         // Hardcoded login for testing (Replace this with actual API)
-        if (email === "reddy@gmail.com" && password === "reddy@123") {
+        if (email === "fastbet@gmail.com" && password === "fastbet@123") {
             localStorage.setItem("isAuthenticated", "true");
             localStorage.setItem("email" ,email);
             setIsAuthenticated(true);
